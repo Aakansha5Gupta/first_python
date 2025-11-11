48b5cce4b5a05ef4c08f458de4c6418eedb68bdb
Aakansha5Gupta <aakanshag87@gmail.com>
2025-11-11 21:55:32 +0530

    First_commit

---
+++ Hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () 
+{
+    printf("Hello World");
+    printf("Name:Aakansha Gupta\n");
+    printf("Age:19\n");
+    printf("University:Shobhit University\n");
+    printf("course:BCA-Cyber Security\n");
+    return 0;
+}
