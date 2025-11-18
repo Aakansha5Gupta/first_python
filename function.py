9f865934fccb8ef5a1d829a8863fdc714fc762fb
Aakansha5Gupta <aakanshag87@gmail.com>
2025-11-18 19:33:48 +0530

    first commit

---
+++ function.py
@@ -0,0 +1,21 @@
+def my_function():
+    print("Aakansha Gupta")
+    print("BCA-CSE")
+    print("This is my first time I am calling my function")
+
+# First function call
+my_function()
+
+a = 3
+b = 2
+print("The sum of a and b:", a + b)
+
+print("It's my second time I call my function")
+my_function()
+
+# Loop
+for i in range(3):
+    print(i)
+
+print("It's my third time I call my function")
+my_function()
