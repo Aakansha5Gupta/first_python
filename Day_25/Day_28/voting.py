b67e989648dede49a07c312e377bb96ee5f61607
aakansha5gupta <aakanshag87@gmail.com>
2025-11-22 18:52:03 +0530

    added more files

---
+++ Day_25/Day_28/voting.py
@@ -0,0 +1,7 @@
+age = int(input("hi guys!enteryour age:"))
+
+if age>=18:
+    print("yes,you are eligible to vote")
+else:
+    print("you are not eligigble to vote")
+        
---
+++ Day_25/table.py
@@ -0,0 +1,2 @@
+for i in range (13,131 , 13):
+    print(i)
