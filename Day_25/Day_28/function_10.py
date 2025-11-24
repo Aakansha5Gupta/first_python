9865c228ad76ab236427a3107ce32466cd240721
aakansha5gupta <aakanshag87@gmail.com>
2025-11-24 19:16:25 +0530

    first commit

---
+++ Day_25/Day_28/function_10.py
@@ -0,0 +1,8 @@
+def divisible_by_10():
+    n=int(input("enter the number:"))
+    for i in range (1,n+1):
+        if i%10==0:
+            print(i)
+
+            #this is my first call 
+divisible_by_10()
